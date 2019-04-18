c15653335a94226c1b16ea6b576fe8d3ea06737a
L. Dolio Durant <dolio@zipcodewilmington.com>
2019-04-18 17:08:33 -0400

    adding status to achievement assignment table

--- database/migrations/2019_04_10_195202_create_achievements_table.php
+++ database/migrations/2019_04_10_195202_create_achievements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAcheivementsTable extends Migration
+class CreateAchievementsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAcheivementsTable extends Migration
      */
     public function up()
     {
-        Schema::create('acheivements', function (Blueprint $table) {
+        Schema::create('achievements', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('status')->nullable();
@@ -29,6 +29,6 @@ class CreateAcheivementsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('acheivements');
+        Schema::dropIfExists('achievements');
     }
 }
--- database/migrations/2019_04_10_195203_create_achievements_assignments_table.php
+++ database/migrations/2019_04_10_195203_create_achievements_assignments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAssignmentsTable extends Migration
+class CreateAchieventsAssignmentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateAssignmentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('acheivements_assignments', function (Blueprint $table) {
+        Schema::create('achievements_assignments', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('student_id')->unsigned();
-            $table->integer('acheivement_id')->unsigned();
-
+            $table->integer('achievement_id')->unsigned();
+            $table->string('status')->nullable();
             $table->foreign('student_id')->references('id')->on('students');
-            $table->foreign('acheivement_id')->references('id')->on('acheivements');
+            $table->foreign('achievement_id')->references('id')->on('achievements');
 
 
-            $table->unique(['student_id','acheivement_id']);
+            $table->unique(['student_id','achievement_id']);
             $table->timestamps();
         });
     }
@@ -34,6 +34,6 @@ class CreateAssignmentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('acheivements_assignments');
+        Schema::dropIfExists('achievements_assignments');
     }
 }
