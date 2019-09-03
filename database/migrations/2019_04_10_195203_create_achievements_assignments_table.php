6d9649450aad7ad3c28d5472427fb99324af29e0
L. Dolio Durant <dolio@zipcodewilmington.com>
2019-09-03 16:46:11 -0400

    typo

--- database/migrations/2019_04_10_195203_create_achievements_assignments_table.php
+++ database/migrations/2019_04_10_195203_create_achievements_assignments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAchieventsAssignmentsTable extends Migration
+class CreateAchievementsAssignmentsTable extends Migration
 {
     /**
      * Run the migrations.
