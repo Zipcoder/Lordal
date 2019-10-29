1f8621b364d9d96a12ce6e582f81927e65e52a23
L. Dolio Durant <dolio@zipcodewilmington.com>
2019-10-29 17:48:35 -0400

    adding js to the overview report

--- resources/js/components/StaffPortal/CohortOverviewReport.vue
+++ resources/js/components/StaffPortal/CohortOverviewReport.vue
@@ -48,4 +48,34 @@
 
 
 <script>
+console.log("self.students");
+
+    export default {
+        data: function() {
+            return {
+                students: [],
+                currentSortDir: 'asc',
+                currentSort: 'id'
+            };
+        },
+        props: [],
+        mounted() {
+            var self = this;
+            window.axios.get(`/api/students`)
+                .then(function(response) {
+                    self.students = response.data;
+                    console.log(self.students);
+
+        //console.log(JSON.stringify(feedProcessed));
+
+                    //var results = self.students.assessments;
+                    
+                });
+        },
+        methods: {
+        },
+        computed: {
+
+        }
+    };
 </script>
